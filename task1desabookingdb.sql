8e3265ee3bfbd761f9435451e687a9c069b3056d
Eddy V <mekatlyu@bitbucket.com>
2019-03-06 10:54:59 -0600

    database of task 1 added

---
+++ task1desabookingdb.sql
@@ -0,0 +1,79 @@
+CREATE DATABASE desabookingdb;
+CREATE USER desabUsr IDENTIFIED BY 'desabRoot';
+GRANT ALL PRIVILEGES ON desabookingdb.* to desabUsr@localhost IDENTIFIED BY 'desabRoot';
+USE desabookingdb AS desabUsr 'desabRoot'
+
+CREATE TABLE `usuario` (
+  `id` BIGINT NOT NULL AUTO_INCREMENT,
+  `nombre` VARCHAR(30) NOT NULL,
+  `apellidos` VARCHAR(30) NOT NULL,
+  `cedula` VARCHAR(30) NOT NULL,
+  `creacion` TIMESTAMP NOT NULL ,
+  `hash`   VARBINARY(32)
+  CONSTRAINT PKusuario PRIMARY KEY(id)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+DELIMITER //
+    CREATE PROCEDURE CREATEusuario(
+    IN p_nombre VARCHAR(30),
+     IN p_apellidos VARCHAR(30),
+     IN p_cedula VARCHAR(30)
+   )
+    BEGIN
+          DECLARE creacion_now TIMESTAMP(4) CURRENT_TIMESTAMP;
+            INSERT INTO usuario(nombre,apellidos,cedula,creacion,hash)
+          VALUES(
+            p_nombre,
+            p_apellidos,
+            p_cedula,
+            creacion_now,
+            MD5(CONCAT('*',p_nombre,p_apellidos,p_cedula,creacion_now)));
+    END;
+    //
+DELIMITER;
+
+DELIMITER //
+    CREATE PROCEDURE READusuario(IN p_id BIGINT)
+    BEGIN
+            SELECT nombre,apellidos,cedula,creacion,hash
+              FROM usuario
+            WHERE id=p_id;
+    END;
+    //
+DELIMITER;
+
+DELIMITER //
+    CREATE PROCEDURE READALLusuario()
+    BEGIN
+            SELECT nombre,apellidos,cedula,creacion,hash
+            FROM usuario
+            WHERE id=p_id;
+    END;
+    //
+DELIMITER;
+
+DELIMITER //
+    CREATE PROCEDURE UPDATEusuario(
+      IN p_nombre VARCHAR(30),
+      IN p_apellidos VARCHAR(30),
+      IN p_cedula VARCHAR(30)
+    )
+    BEGIN
+          DECLARE creacion_now TIMESTAMP(4) CURRENT_TIMESTAMP;
+            UPDATE usuario
+            SET nombre =p_nombre,
+             apellidos=p_apellidos,
+             cedula=p_cedula,
+             creacion=creacion_now,
+             hash=MD5(CONCAT('*',p_nombre,p_apellidos,p_cedula,creacion_now));
+    END;
+    //
+DELIMITER;
+
+DELIMITER //
+    CREATE PROCEDURE DELETEusuario(IN p_id BIGINT)
+    BEGIN
+            DELETE FROM usuario WHERE id=p_id;
+    END;
+    //
+DELIMITER;
