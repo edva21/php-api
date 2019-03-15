c98db0ddf5829dc77feff04b2872ee039b142d49
Eddy <eddy.valverde.garro@gmail.com>
2019-03-14 22:53:45 -0600

    added delete

---
+++ apiV2/item/delete.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: eddy
+ * Date: 12/03/19
+ * Time: 05:19 PM
+ */
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+// include database and object file
+include_once '../config/Database.php';
+include_once '../objects/Item.php';
+
+// get database connection
+$database = new Database();
+$db = $database->getConnection();
+
+// prepare product object
+$Item = new Item($db);
+
+// get product id
+$data = json_decode(file_get_contents("php://input"));
+
+// set product id to be deleted
+$Item->id = $data->id;
+
+// delete the product
+if($Item->delete()){
+
+    // set response code - 200 ok
+    http_response_code(200);
+
+    // tell the user
+    echo json_encode(array("message" => "Item was deleted."));
+}
+
+// if unable to delete the product
+else{
+
+    // set response code - 503 service unavailable
+    http_response_code(503);
+
+    // tell the user
+    echo json_encode(array("message" => "Unable to delete product."));
+}
+?>
